5e4e5c952f7261c5fa9fab6744dec6d2ce5998c4
Fatih <fatakhillah.khaqo@gmail.com>
2024-10-17 10:45:33 +0700

    Initial Commit

---
+++ SpeechRecognitionPlugin/SpeechRecognitionPlugin.h
@@ -0,0 +1,18 @@
+//
+//  SpeechRecognitionPlugin.h
+//  SpeechRecognitionPlugin
+//
+//  Created by Fatakhillah Khaqo on 17/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpeechRecognitionPlugin.
+FOUNDATION_EXPORT double SpeechRecognitionPluginVersionNumber;
+
+//! Project version string for SpeechRecognitionPlugin.
+FOUNDATION_EXPORT const unsigned char SpeechRecognitionPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpeechRecognitionPlugin/PublicHeader.h>
+
+
